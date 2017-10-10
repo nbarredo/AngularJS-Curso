9bb034e477c87e0f36087a42cb540a64c9a07bf6
Nicolas Barredo <nbarredo@overactiveinc.com>
2017-10-09 23:06:26 -0300

    ej clase 1

--- Clase-1/Controller/Clase1Controller.js
+++ Clase-1/Controller/Clase1Controller.js
@@ -1,4 +1,13 @@
 ﻿app.controller('Clase1Controller', function ($scope) {
 
     $scope.Name ='Nico'
+
+    var person = {
+        id:1,
+        firstName:"John",
+        lastName:"Doe",
+        ip:"192.168.0.1",
+        gender:"male"
+    };
+    $scope.person = person;
 });
