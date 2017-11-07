e69ec66f8d9a80872df5ac3d1c5db14da7ba12cb
Nicolas Barredo <nbarredo@overactiveinc.com>
2017-11-06 21:05:45 -0300

    watch

--- Clase-1/Controller/detailsController.js
+++ Clase-1/Controller/detailsController.js
@@ -1,12 +1,17 @@
 ﻿app.controller('DetailsController', ['$scope', 'personService', '$routeParams', function ($scope, personService, $routeParams) {
     $scope.person = {};
+   
     var personId = $routeParams.personId;
     var promise = personService.get(personId);
     promise.then(function (data) {
+        
         $scope.person = data;
-
+        $scope.person.dirty = false;
+       
     }, function (error) {
 
-    }); 
-     
+        });  
+    $scope.$watch('person.FirstName', function (newValue, oldValue) { 
+        $scope.person.oldName = oldValue; 
+    });
 }]);
